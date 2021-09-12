4839e86a7c0eafe5b30cb4a1c15b2228b3b82902
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-09-12 12:13:15 +0200

    Create base components

---
+++ components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+      <a href="https://github.com/MoritzKronberger/portfolio">git</a>
+      <a href="https://de.linkedin.com/">li</a>
+      <p>© Moritz Kronberger 2021</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
