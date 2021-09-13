b923cf55e0f888f71b72c65c47627ba1d9f9634a
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-09-13 16:13:52 +0200

    Add post slug

---
+++ pages/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+      <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+    export default {
+        async asyncData({ $content, params }){
+            const post = await $content('projects/markdown', params.slug).fetch();
+            return { post }
+        }
+    }
+</script>
+
+<style>
+
+</style>
