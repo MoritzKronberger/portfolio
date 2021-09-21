083894100fe2e791429ee5f11750c5396a46f707
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-09-21 16:55:37 +0200

    Revert "Add dist folder to git for deployment"
    This reverts commit 51da724f3766ef733a3342f5eff37f9063508329.

--- dist/_nuxt/static/1632232074/design-project/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632232074",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdesign-project",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f20370e0"}}}}(null));
