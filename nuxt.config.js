a606dd7975485aed3f460528775bf15711b56d4d
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-09-21 16:13:01 +0200

    Add darkmode compatible svg favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       { rel: 'apple-touch-icon', type: 'image/png', sizes:'180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes:'16x16', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/png', sizes:'32x32', href: '/favicon-32x32.png' }
